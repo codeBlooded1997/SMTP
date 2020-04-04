b6fa7f199759106198b44b690b65f15c237c0966
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-04 09:42:29 -0400

    Updated send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -2,15 +2,15 @@
 import smtplib
 
 # Which email is this being send from
-sender_email = 'AaronSMTPsender@gmail.com'
+sender_email = 'ariannumber1@gmail.com'
 
 # Password so we can log in to the senders account
-password = 'senderpassword'
+password = 'hooman12'
 
 # Who is this email going to ve sent to
-recipient_email = 'JohnSMTPreceiver1@gmail.com'
+recipient_email = 'arianaghnaei@gmail.com'
 
-#
+
 email_text = '''
     Dummy text. Yo whats's up.
 '''
@@ -21,10 +21,19 @@ def send_email():
     # Common smtp ports: 25 or 2525 or 587
     server = smtplib.SMTP('smtp.gmail.com', 587)  # hotmail: smtp.live.com   # aol: smtp.aol.com  #yahoo: smtp.mail.yahoo.com
 
+    # I asicly allow us encript what we are sending to server from this point on
+    # Turn on Transport layer security. All commands after this will be encrypted.
+    server.starttls()
+
     # Login to the senders email
     server.login(sender_email, password)
-    pass
 
+    # Send the email
+    server.sendmail(sender_email, recipient_email, email_text)
 
+    # Cleanup
+    server.quit()
+
+    
 # Calling the function to send emails
 send_email()
